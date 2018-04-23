987d97a92910c4d9b41da26686ee118b26e15fa5
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace SchedulerResourcesTreeColors
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            DevExpress.XtraScheduler.TimeRuler timeRuler1 = new DevExpress.XtraScheduler.TimeRuler();
+            DevExpress.XtraScheduler.TimeRuler timeRuler2 = new DevExpress.XtraScheduler.TimeRuler();
+            this.schedulerControl1 = new DevExpress.XtraScheduler.SchedulerControl();
+            this.schedulerStorage1 = new DevExpress.XtraScheduler.SchedulerStorage(this.components);
+            this.taskDependenciesBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.tasksDataSet1 = new SchedulerResourcesTreeColors.TasksDataSet();
+            this.tasksBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.resourcesBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.resourcesTree1 = new DevExpress.XtraScheduler.UI.ResourcesTree();
+            this.resourceTreeColumn1 = new DevExpress.XtraScheduler.Native.ResourceTreeColumn();
+            this.tasksTableAdapter = new SchedulerResourcesTreeColors.TasksDataSetTableAdapters.TasksTableAdapter();
+            this.taskDependenciesTableAdapter = new SchedulerResourcesTreeColors.TasksDataSetTableAdapters.TaskDependenciesTableAdapter();
+            this.resourcesTableAdapter = new SchedulerResourcesTreeColors.TasksDataSetTableAdapters.ResourcesTableAdapter();
+            this.resourceTreeColumn2 = new DevExpress.XtraScheduler.Native.ResourceTreeColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.schedulerControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.schedulerStorage1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.taskDependenciesBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tasksDataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tasksBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.resourcesBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.resourcesTree1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // schedulerControl1
+            // 
+            this.schedulerControl1.ActiveViewType = DevExpress.XtraScheduler.SchedulerViewType.Gantt;
+            this.schedulerControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.schedulerControl1.GroupType = DevExpress.XtraScheduler.SchedulerGroupType.Resource;
+            this.schedulerControl1.Location = new System.Drawing.Point(206, 12);
+            this.schedulerControl1.Name = "schedulerControl1";
+            this.schedulerControl1.Size = new System.Drawing.Size(572, 332);
+            this.schedulerControl1.Start = new System.DateTime(2008, 8, 31, 0, 0, 0, 0);
+            this.schedulerControl1.Storage = this.schedulerStorage1;
+            this.schedulerControl1.TabIndex = 0;
+            this.schedulerControl1.Text = "schedulerControl1";
+            this.schedulerControl1.Views.DayView.TimeRulers.Add(timeRuler1);
+            this.schedulerControl1.Views.GanttView.ResourcesPerPage = 6;
+            this.schedulerControl1.Views.GanttView.ShowResourceHeaders = false;
+            this.schedulerControl1.Views.WorkWeekView.TimeRulers.Add(timeRuler2);
+            // 
+            // schedulerStorage1
+            // 
+            this.schedulerStorage1.AppointmentDependencies.DataSource = this.taskDependenciesBindingSource;
+            this.schedulerStorage1.AppointmentDependencies.Mappings.DependentId = "Dependent";
+            this.schedulerStorage1.AppointmentDependencies.Mappings.ParentId = "Id";
+            this.schedulerStorage1.AppointmentDependencies.Mappings.Type = "Type";
+            this.schedulerStorage1.Appointments.DataSource = this.tasksBindingSource;
+            this.schedulerStorage1.Appointments.Mappings.AppointmentId = "Id";
+            this.schedulerStorage1.Appointments.Mappings.End = "EndTime";
+            this.schedulerStorage1.Appointments.Mappings.Label = "Label";
+            this.schedulerStorage1.Appointments.Mappings.ResourceId = "ResourceId";
+            this.schedulerStorage1.Appointments.Mappings.Start = "StartTime";
+            this.schedulerStorage1.Appointments.Mappings.Subject = "Subject";
+            this.schedulerStorage1.Resources.DataSource = this.resourcesBindingSource;
+            this.schedulerStorage1.Resources.Mappings.Caption = "Description";
+            this.schedulerStorage1.Resources.Mappings.Color = "Color";
+            this.schedulerStorage1.Resources.Mappings.Id = "Id";
+            this.schedulerStorage1.Resources.Mappings.ParentId = "ParentId";
+            // 
+            // taskDependenciesBindingSource
+            // 
+            this.taskDependenciesBindingSource.DataMember = "TaskDependencies";
+            this.taskDependenciesBindingSource.DataSource = this.tasksDataSet1;
+            // 
+            // tasksDataSet1
+            // 
+            this.tasksDataSet1.DataSetName = "TasksDataSet";
+            this.tasksDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // tasksBindingSource
+            // 
+            this.tasksBindingSource.DataMember = "Tasks";
+            this.tasksBindingSource.DataSource = this.tasksDataSet1;
+            // 
+            // resourcesBindingSource
+            // 
+            this.resourcesBindingSource.DataMember = "Resources";
+            this.resourcesBindingSource.DataSource = this.tasksDataSet1;
+            // 
+            // resourcesTree1
+            // 
+            this.resourcesTree1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.resourcesTree1.Columns.AddRange(new DevExpress.XtraTreeList.Columns.TreeListColumn[] {
+            this.resourceTreeColumn1,
+            this.resourceTreeColumn2});
+            this.resourcesTree1.Location = new System.Drawing.Point(9, 12);
+            this.resourcesTree1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.resourcesTree1.Name = "resourcesTree1";
+            this.resourcesTree1.SchedulerControl = this.schedulerControl1;
+            this.resourcesTree1.Size = new System.Drawing.Size(192, 332);
+            this.resourcesTree1.TabIndex = 1;
+            this.resourcesTree1.CustomDrawNodeCell += new DevExpress.XtraTreeList.CustomDrawNodeCellEventHandler(this.resourcesTree1_CustomDrawNodeCell);
+            // 
+            // resourceTreeColumn1
+            // 
+            this.resourceTreeColumn1.Caption = "Description";
+            this.resourceTreeColumn1.FieldName = "Description";
+            this.resourceTreeColumn1.Name = "resourceTreeColumn1";
+            this.resourceTreeColumn1.Visible = true;
+            this.resourceTreeColumn1.VisibleIndex = 0;
+            // 
+            // tasksTableAdapter
+            // 
+            this.tasksTableAdapter.ClearBeforeFill = true;
+            // 
+            // taskDependenciesTableAdapter
+            // 
+            this.taskDependenciesTableAdapter.ClearBeforeFill = true;
+            // 
+            // resourcesTableAdapter
+            // 
+            this.resourcesTableAdapter.ClearBeforeFill = true;
+            // 
+            // resourceTreeColumn2
+            // 
+            this.resourceTreeColumn2.Caption = "resourceTreeColumn2";
+            this.resourceTreeColumn2.FieldName = "Id";
+            this.resourceTreeColumn2.Name = "resourceTreeColumn2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(790, 387);
+            this.Controls.Add(this.resourcesTree1);
+            this.Controls.Add(this.schedulerControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.schedulerControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.schedulerStorage1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.taskDependenciesBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tasksDataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tasksBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.resourcesBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.resourcesTree1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraScheduler.SchedulerControl schedulerControl1;
+        private DevExpress.XtraScheduler.SchedulerStorage schedulerStorage1;
+        private DevExpress.XtraScheduler.UI.ResourcesTree resourcesTree1;
+        private TasksDataSet tasksDataSet1;
+        private DevExpress.XtraScheduler.Native.ResourceTreeColumn resourceTreeColumn1;
+        private System.Windows.Forms.BindingSource tasksBindingSource;
+        private SchedulerResourcesTreeColors.TasksDataSetTableAdapters.TasksTableAdapter tasksTableAdapter;
+        private System.Windows.Forms.BindingSource taskDependenciesBindingSource;
+        private SchedulerResourcesTreeColors.TasksDataSetTableAdapters.TaskDependenciesTableAdapter taskDependenciesTableAdapter;
+        private System.Windows.Forms.BindingSource resourcesBindingSource;
+        private SchedulerResourcesTreeColors.TasksDataSetTableAdapters.ResourcesTableAdapter resourcesTableAdapter;
+        private DevExpress.XtraScheduler.Native.ResourceTreeColumn resourceTreeColumn2;
+    }
+}
+
